0375decc674f18b32f6f7773e72910479b8e0447
aazharnaeem <azhar.naeem@quanrio.com>
2023-02-01 16:21:18 +0500

    updates

--- src/Forms/Review/index.tsx
+++ src/Forms/Review/index.tsx
@@ -1,32 +1,43 @@
 import React from 'react'
 
-export const Review:React.FC<any> =({values})=>{
-    return(
+export const Review: React.FC<any> = ({ values }) => {
+    return (
         <div>
             <table
-            style={{margin:'0 auto'}}
+                style={{ margin: '0 auto', border: "solid 1px" }}
             >
                 <tr>
-                    <td>First Name</td>
-                    <td> Last Name</td>
-                    <td>Age</td>
-                    <td>Number</td>
-                    <td>Email</td>
-                    <td>Country</td>
-                    <td>City</td>
-                    <td>Addres</td>
-                    <td>Street</td>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>First Name</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}> Last Name</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>Age</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>Number</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>Email</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>Country</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>City</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>Addres</th>
+                    <th style={{ border: "1px solid black", padding: "5px" }}>Street</th>
                 </tr>
                 <tr>
-                    <td>{values.firstName}</td>
-                    <td>{values.lastName}</td>
-                    <td>{values.age}</td>
-                    <td>{values.number}</td>
-                    <td>{values.email}</td>
-                    <td>{values.country}</td>
-                    <td>{values.city}</td>
-                    <td>{values.address}</td>
-                    <td>{values.street}</td>
+                    <td style={{ border: "1px solid black" }}>{values.firstName}</td>
+                    <td style={{ border: "1px solid black" }}>{values.lastName}</td>
+                    <td style={{ border: "1px solid black" }}>{values.age}</td>
+                    <td style={{ border: "1px solid black" }}>{values.number}</td>
+                    <td style={{ border: "1px solid black" }}>{values.email}</td>
+                    <td style={{ border: "1px solid black" }}>{values.country}</td>
+                    <td style={{ border: "1px solid black" }}>{values.city}</td>
+                    <td style={{ border: "1px solid black" }}>{values.address}</td>
+                    <td style={{ border: "1px solid black" }}>{values.street}</td>
+                </tr>
+                <tr>
+                    <td style={{ border: "1px solid black" }}>{values.firstName}</td>
+                    <td style={{ border: "1px solid black" }}>{values.lastName}</td>
+                    <td style={{ border: "1px solid black" }}>{values.age}</td>
+                    <td style={{ border: "1px solid black" }}>{values.number}</td>
+                    <td style={{ border: "1px solid black" }}>{values.email}</td>
+                    <td style={{ border: "1px solid black" }}>{values.country}</td>
+                    <td style={{ border: "1px solid black" }}>{values.city}</td>
+                    <td style={{ border: "1px solid black" }}>{values.address}</td>
+                    <td style={{ border: "1px solid black" }}>{values.street}</td>
                 </tr>
             </table>
         </div>
